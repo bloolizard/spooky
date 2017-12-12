72af015dc5f1450f4db24df69ee118dbd6e7f409
Edwin Villanueva <edwincv0@gmail.com>
2017-12-12 06:51:44 -0800

    Add Train/Test Data Dataframe

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ path = "/home/edwin/Developer/spooky/data/"
 train_path = path + "train.csv"
 test_path = path + "test.csv"
 
+# Train Data
+# id, text, author
+train_data_df = pd.read_csv(train_path)
 
+# Test Data
+# id, text
+test_data_df = pd.read_csv(test_path)
+
+# Sample Submission
+# id, EAP, HPL, MWS

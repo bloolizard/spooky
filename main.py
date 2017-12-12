c738222647dc97baa801649cf16e2d366789149f
Edwin Villanueva <edwincv0@gmail.com>
2017-12-12 06:42:04 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+path = "/home/edwin/Developer/spooky/data/"
+train_path = path + "train.csv"
+test_path = path + "test.csv"
+
+

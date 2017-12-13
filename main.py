84943a894d8059bf45241365027bb0e798f155a9
Edwin Villanueva <edwincv0@gmail.com>
2017-12-12 17:49:45 -0800

    Added NLTK

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-path = "/home/edwin/Developer/spooky/data/"
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import nltk
+
+path = "~/Developer/spooky/data/"
 train_path = path + "train.csv"
 test_path = path + "test.csv"
 
@@ -18,3 +26,11 @@ test_data_df = pd.read_csv(test_path)
 
 # Sample Submission
 # id, EAP, HPL, MWS
+
+test_data_df
+
+word_to_ix = {}
+
+
+total_words = train_data_df.text.str.split(" ")
+

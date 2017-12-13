0254891bf8dc1aa8b7ec8065e2f80bb1ae05a0b7
Edwin Villanueva <edwincv0@gmail.com>
2017-12-12 21:16:07 -0800

    Added transforms into Vector

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ path = "~/Developer/spooky/data/"
 train_path = path + "train.csv"
 test_path = path + "test.csv"
 
+from __future__ import division
+
+from sklearn.feature_extraction.text import CountVectorizer
+
 # Train Data
 # id, text, author
 train_data_df = pd.read_csv(train_path)
@@ -33,4 +37,28 @@ word_to_ix = {}
 
 
 total_words = train_data_df.text.str.split(" ")
+
+len(total_words)
+
+sentence = train_data_df.text[0].split(" ")
+len(sentence)
+len(set(sentence))
+len(set(sentence)) / len(sentence)
+
+sentence = train_data_df.text[0].split(" ")
+vocab = set(sentence)
+vocab
+
+word_to_ix = {word: i for i, word in enumerate(vocab) }
+
+# using NLTK
+sentence = train_data_df.text[0]
+first_sentence = nltk.word_tokenize(sentence)
+
+stopwords = nltk.corpus.stopwords.words('english')
+sentence_filtered = [word for word in first_sentence if word.lower() not in stopwords]
+
+vectorizer = CountVectorizer()
+sentence_transform = vectorizer.fit_transform(sentence_filtered)
+sentence_transform
 
